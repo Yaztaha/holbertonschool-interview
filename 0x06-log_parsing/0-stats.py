2a82d62a3cc5047bd4bcae6b0410cbe9d83bed35
yaztaha <yazi.taha@gmail.com>
2021-06-03 00:24:10 +0000

    stats_log

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -2,39 +2,38 @@
 """ log steeam parsing by line  """
 import sys
 
-dict_codes = {'200': 0, '301': 0, '400': 0, '401': 0,
-              '403': 0, '404': 0, '405': 0, '500': 0}
-total_size = 0
-num_lines = 0
+import sys
+
+count = 1
+sum_size = 0
+status_code = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0, '404': 0,
+               '405': 0, '500': 0}
 
 try:
     for line in sys.stdin:
-        line_list = line.split(" ")
+        ln = line.split()
 
-        if len(line_list) > 2:
-            size = line_list[-1]
-            status_code = line_list[-2]
+        if len(ln) > 2:
+            status = ln[-2]
+            size = int(ln[-1])
 
-            if status_code in dict_codes.keys():
-                dict_codes[status_code] += 1
+            sum_size += size
+            if status in status_code:
+                status_code[status] += 1
 
-            num_lines += 1
-            total_size += int(size)
+        if count % 10 == 0:
+            print("File size: {}".format(sum_size))
+            for key in sorted(status_code.keys()):
+                if status_code[key] != 0:
+                    print("{}: {}".format(key, status_code[key]))
 
-        if num_lines == 10:
-            print("File size: {}".format(total_size))
-
-            for key, value in sorted(dict_codes.items()):
-                if value > 0:
-                    print("{}: {}".format(key, value))
-            num_lines = 0
+        count += 1
 
 except Exception:
     pass
 
 finally:
-    print("File size: {}".format(total_size))
-
-    for key, value in sorted(dict_codes.items()):
-        if value > 0:
-            print("{}: {}".format(key, value))
+    print("File size: {}".format(sum_size))
+    for key in sorted(status_code.keys()):
+        if status_code[key] != 0:
+            print("{}: {}".format(key, status_code[key]))
